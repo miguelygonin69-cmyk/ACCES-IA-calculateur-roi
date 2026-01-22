7e01d5663b9faaae05ff85eb712aabdab6df1ac0
miguelygonin69-cmyk <miguel.ygonin69@gmail.com>
2026-01-22 18:45:54 +0100

    feat: Remove StrictMode from root rendering
    StrictMode has been removed from the root rendering of the application. This simplifies the entry point and avoids potential double-rendering issues during development or in certain environments.

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
@@ -8,8 +7,4 @@ if (!rootElement) {
 }
 
 const root = createRoot(rootElement);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
